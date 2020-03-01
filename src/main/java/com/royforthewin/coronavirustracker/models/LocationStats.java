c6ad87cc30c2d6ffd56c2eceda4ec4f392dca8bc
Roy <roynny51@gmail.com>
2020-03-01 01:52:42 -0300

    done

---
+++ src/main/java/com/royforthewin/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,19 @@
+package com.royforthewin.coronavirustracker.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
+
+
+}
